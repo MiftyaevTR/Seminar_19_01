b476449694d9954dcc3d1e42ade0aa3486fbe339
Timur <t926661227@yandex.ru>
2023-01-23 15:44:11 +0300

    final

--- Program.cs
+++ Program.cs
@@ -382,4 +382,65 @@ int result =SearchElement(myArray);
 if (result==-1) Console.WriteLine("Числа c такими индексами в массиве нет");
 else Console.WriteLine("Значение элемента: " + result);
 */
+//Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+/*
+int[,] CreateRandom2dArray ()
+{
+    Console.WriteLine("Input a numbers of rows: ");
+    int rows =Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Input a numbers of columns: ");
+    int columns =Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Input a min of element: ");
+    int min =Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Input a max of element: ");
+    int max =Convert.ToInt32(Console.ReadLine());
+
+    int[,] array=new int[rows,columns];
+
+    for (int i=0; i<rows; i++)
+        for (int j=0; j<columns; j++)
+            array[i,j]= new Random().Next(min, max+1);
+    return array;
+}
+
+void Show2dArray (int[,] array)
+{
+     for (int i=0; i<array.GetLength(0); i++)
+     {
+        for (int j=0; j<array.GetLength(1); j++)
+            Console.Write(array[i,j]+ " ");
+        Console.WriteLine();
+     }
+     Console.WriteLine();
+}
+
+double [] AvgColumnsElements (int[,] array)
+{
+    double[] res=new double[array.GetLength(1)]; 
+    for (int j=0; j<array.GetLength(1); j++)
+    {   
+        double sum=0;
+        for (int i=0; i<array.GetLength(0); i++)
+            sum+=array[i,j];
+        res[j]=Math.Round((sum/array.GetLength(0)),2);
+    }   
+    return res;
+}
+
+void ShowArray (double[] array)
+{
+    for (int i=0; i<array.Length; i++)
+        Console.Write(array[i] + "; ");
+    Console.WriteLine();
+}
+
+int[,] myArray= CreateRandom2dArray();
+Show2dArray(myArray);
+double[] result=AvgColumnsElements(myArray);
+Console.WriteLine("Среднее арифметическое каждого столбца: ");
+ShowArray(result);
+*/
+
+
+
 
